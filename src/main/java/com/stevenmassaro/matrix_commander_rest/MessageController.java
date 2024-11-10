51daf6004ec3f9dd9682a0bc7503bd751b8c586e
Steven Massaro <steven.massaro.web@gmail.com>
2024-11-09 18:50:27 -0600

    support an alternative JSON request body

--- src/main/java/com/stevenmassaro/matrix_commander_rest/MessageController.java
+++ src/main/java/com/stevenmassaro/matrix_commander_rest/MessageController.java
@@ -1,9 +1,11 @@
 package com.stevenmassaro.matrix_commander_rest;
 
+import com.stevenmassaro.matrix_commander_rest.model.MessageRequest;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,8 +19,30 @@ class MessageController {
     @Value("#{'${ROOMS}'.split(';')}")
     private List<String> rooms;
 
-    @RequestMapping(path = "/message/{room}", method = RequestMethod.POST)
-    int sendMessage(@PathVariable("room") String room, @RequestParam MultiValueMap message) throws IOException {
+
+    @RequestMapping(path = "/message/{room}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public int sendMessageForm(
+            @PathVariable("room") String room,
+            @RequestParam MultiValueMap<String, String> params
+    ) throws IOException {
+        String message = params.getFirst("message");
+        return sendMessageInternal(room, message);
+    }
+
+    @RequestMapping(path = "/message/{room}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public int sendMessageJson(
+            @PathVariable("room") String room,
+            @RequestBody MessageRequest messageRequest
+    ) throws IOException {
+        String message = messageRequest.getMessage();
+        return sendMessageInternal(room, message);
+    }
+
+    private int sendMessageInternal(String room, String message) throws IOException {
+        if (message == null || message.trim().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Message cannot be empty.");
+        }
+
         String roomId = getRoomIdFromName(room);
         if (roomId == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Room not found.");
@@ -26,7 +50,7 @@ class MessageController {
         String command = "/bin/python3 /app/matrix_commander/matrix-commander -s /data/store -c /data/credentials.json -r " + roomId + " -m";
 
         CommandLine cmdLine = CommandLine.parse(command);
-        cmdLine.addArgument(String.valueOf(message.getFirst("message")), false);
+        cmdLine.addArgument(message, false);
         DefaultExecutor executor = DefaultExecutor.builder().get();
         return executor.execute(cmdLine);
     }
---
+++ src/main/java/com/stevenmassaro/matrix_commander_rest/model/MessageRequest.java
@@ -0,0 +1,8 @@
+package com.stevenmassaro.matrix_commander_rest.model;
+
+import lombok.Data;
+
+@Data
+public class MessageRequest {
+    private String message;
+}
